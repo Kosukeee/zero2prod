860659642ddfa387442e0d4709067c663e4cef92
Kosuke Muramatsu <kousuke.muramatsu@gmail.com>
2025-08-13 19:13:18 +0400

    Clean up test

--- migrations/20250811122720_create_subscription_tokens_table.sql
+++ migrations/20250811122720_create_subscription_tokens_table.sql
@@ -2,7 +2,7 @@
 -- Create Subscription Tokens Table
 CREATE TABLE subscription_tokens(
   subscription_token TEXT NOT NULL,
-  subscription_id uuid NOT NULL
+  subscriber_id uuid NOT NULL
     REFERENCES subscriptions (id),
   PRIMARY KEY (subscription_token)
 );
