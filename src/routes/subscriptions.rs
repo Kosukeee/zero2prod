ab5a4680d501af5da5fc0525ab478461dde2a238
Kosuke Muramatsu <kousuke.muramatsu@gmail.com>
2025-08-13 10:28:15 +0400

    Fix test cases

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -4,6 +4,7 @@ use sqlx::PgPool;
 use uuid::Uuid;
 use crate::domain::{NewSubscriber, SubscriberName, SubscriberEmail};
 use crate::email_client::EmailClient;
+use crate::startup::ApplicationBaseUrl;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -29,7 +30,7 @@ impl TryFrom<FormData> for NewSubscriber {
 
 #[tracing::instrument(
   name = "Adding a new subscriber",
-  skip(form, pool, email_client),
+  skip(form, pool, email_client, base_url),
   fields(
     subscriber_email = %form.email,
     subscriber_name = %form.name
@@ -39,6 +40,7 @@ pub async fn subscribe(
   form: web::Form<FormData>,
   pool: web::Data<PgPool>,
   email_client: web::Data<EmailClient>,
+  base_url: web::Data<ApplicationBaseUrl>,
 ) -> HttpResponse {
   let new_subscriber = match form.0.try_into() {
     Ok(form) => form,
@@ -47,21 +49,11 @@ pub async fn subscribe(
   if insert_subscriber(&pool, &new_subscriber).await.is_err() {
     return HttpResponse::InternalServerError().finish();
   }
-  let confirmation_link = "https://there-is-no-such-domain.com/subscriptions/confirm";
-  if email_client
-    .send_email(
-      new_subscriber.email,
-      "Welcome!",
-      &format!(
-        "Welcome to our newsletter!<br />\
-        Click <a href=\"{}\">here</a> to confirm your subscription.",
-        confirmation_link
-      ),
-      &format!(
-        "Welcome to our newsletter!\nVisit {} to confirm your subscription.",
-        confirmation_link
-      ),
-    )
+  if send_confirmation_email(
+    &email_client,
+    new_subscriber,
+    &base_url.0
+  )
     .await
     .is_err()
   {
@@ -70,11 +62,41 @@ pub async fn subscribe(
   HttpResponse::Ok().finish()
 }
 
+#[tracing::instrument(
+  name = "Send a confirmation email to a new subscriber",
+  skip(email_client, new_subscriber)
+)]
+pub async fn send_confirmation_email(
+  email_client: &EmailClient,
+  new_subscriber: NewSubscriber,
+  base_url: &str,
+) -> Result<(), reqwest::Error> {
+  let confirmation_link = format!("{}/subscriptions/confirm?subscription_token=mytoken", base_url);
+  let plain_body = format!(
+    "Welcome to our newsletter!\nVisit {} to confirm your subscription.",
+    confirmation_link
+  );
+  let html_body = format!(
+    "Welcome to our newsletter!<br />\
+    Click <a href=\"{}\">here</a> to confirm your subscription.",
+    confirmation_link
+  );
+
+  email_client
+    .send_email(
+      new_subscriber.email,
+      "Welcome!",
+      &html_body,
+      &plain_body,
+    )
+    .await
+}
+
 #[tracing::instrument(name = "Saving new subscriber details in the database", skip(new_subscriber, pool))]
 pub async fn insert_subscriber(pool: &PgPool, new_subscriber: &NewSubscriber) -> Result<(), sqlx::Error> {
   sqlx::query!(
     r#"INSERT INTO subscriptions (id, email, name, subscribed_at, status)
-    VALUES ($1, $2, $3, $4, 'confirmed')"#,
+    VALUES ($1, $2, $3, $4, 'pending_confirmation')"#,
     Uuid::new_v4(),
     new_subscriber.email.as_ref(),
     new_subscriber.name.as_ref(),
--- src/startup.rs
+++ src/startup.rs
@@ -1,4 +1,4 @@
-use crate::routes::{health_check, subscribe};
+use crate::routes::{health_check, subscribe, confirm};
 use actix_web::dev::Server;
 use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
@@ -13,6 +13,9 @@ pub struct Application {
   port: u16,
   server: Server,
 }
+
+#[derive(Clone)]
+pub struct ApplicationBaseUrl(pub String);
 
 impl Application {
   pub async fn build(configuration: Settings) -> Result<Self, std::io::Error> {
@@ -36,7 +39,12 @@ impl Application {
     );
     let listener = TcpListener::bind(address)?;
     let port = listener.local_addr().unwrap().port();
-    let server = run(listener, connection_pool, email_client)?;
+    let server = run(
+      listener,
+      connection_pool,
+      email_client,
+      ApplicationBaseUrl(configuration.application.base_url),
+    )?;
 
     Ok(Self { port, server })
   }
@@ -62,6 +70,7 @@ pub fn run(
   listener: TcpListener,
   db_pool: PgPool,
   email_client: EmailClient,
+  base_url: ApplicationBaseUrl,
 ) -> Result<Server, std::io::Error> {
   let db_pool = Data::new(db_pool);
   let email_client = Data::new(email_client);
@@ -70,8 +79,10 @@ pub fn run(
       .wrap(TracingLogger::default())
       .route("/health_check", web::get().to(health_check))
       .route("/subscriptions", web::post().to(subscribe))
+      .route("/subscriptions/confirm", web::get().to(confirm))
       .app_data(db_pool.clone())
       .app_data(email_client.clone())
+      .app_data(Data::new(base_url.clone()))
   })
   .listen(listener)?
   .run();
